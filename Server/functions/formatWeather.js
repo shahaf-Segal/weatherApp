199094cdee833f4d8b4d0a93c70effc15feeb8b3
shahaf-Segal <shahaf100@outlook.co.il>
2024-08-08 00:50:20 +0300

    created get formatted forecast

--- Server/functions/formatWeather.js
+++ Server/functions/formatWeather.js
@@ -1,5 +1,30 @@
 const { getForecastArray } = require("./extractForecast");
 
-const getFormattedForecast = (weatherForecastObj) => {
-  const ForecastArr = getForecastArray(weatherForecastObj);
+const getCurrentFromWeather = (weatherForecastObj) => {
+  const { last_updated, temp_c, condition, wind_kph, precip_mm, humidity } =
+    weatherForecastObj.current;
+  return {
+    last_updated,
+    temp_c,
+    condition: condition.text,
+    wind_kph,
+    precip_mm,
+    humidity,
+  };
 };
+
+const getFormattedForecast = (weatherForecastObj) => {
+  const { location } = weatherForecastObj;
+  const ForecastArr = getForecastArray(weatherForecastObj);
+  const newLocation = {
+    name: location.name,
+    country: location.country,
+    lat: location.lat,
+    lon: location.lon,
+  };
+  const newCurrent = getCurrentFromWeather(weatherForecastObj);
+
+  return { location: newLocation, current: newCurrent, forecast: ForecastArr };
+};
+
+module.exports = { getFormattedForecast };
