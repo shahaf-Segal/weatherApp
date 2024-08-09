c57d3a587607427e8ec9e7d8c6e2254cf3175e40
shahaf-Segal <shahaf100@outlook.co.il>
2024-08-09 20:31:58 +0300

    finished search forecast

--- client/src/components/SearchForecast/SearchForecast.jsx
+++ client/src/components/SearchForecast/SearchForecast.jsx
@@ -1,14 +1,32 @@
+import { useEffect, useState } from "react";
+import { convertDateFormat } from "../../functions/dateToText";
 import Logo from "../Logo/Logo";
+import SearchBar from "./SearchBar/SearchBar";
 import styles from "./SearchForecast.module.css";
 
-function SearchForecast() {
+function SearchForecast({ searchFunc, locationData }) {
+  const [infoText, setInfoText] = useState("No location entered");
+  useEffect(() => {
+    if (locationData) {
+      setInfoText(
+        `longitude:${locationData.lon}  ,  latitude: ${locationData.lat} 
+       ${convertDateFormat(locationData.localtime)}`
+      );
+    }
+  }, [locationData]);
+
   return (
     <div className={styles["search-forecast"]}>
       <div className={styles["logo-container"]}>
         <Logo />
       </div>
-      <div className={styles["search-inner-container"]}></div>
-      <section>Use our weather app to see the weather around the world</section>
+      <div className={styles["search-inner-container"]}>
+        <div className={styles["intro-text"]}>
+          Use our weather app to see the weather around the world
+        </div>
+        <SearchBar searchFunc={searchFunc} />
+        <article className={styles["info-text"]}>{infoText}</article>
+      </div>
     </div>
   );
 }
