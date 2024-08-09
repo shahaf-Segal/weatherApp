9e8770692696c4a5c33894d35e330ab44dd1ad3c
shahaf-Segal <shahaf100@outlook.co.il>
2024-08-09 14:39:13 +0300

    start styling forecast page

---
+++ client/src/components/pages/ForecastPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Logo from "../Logo/Logo";
+import SearchForecast from "../SearchForecast/SearchForecast";
+import styles from "./ForecastPage.module.css";
+
+function ForecastPage() {
+  return (
+    <div className={styles["forecast-container"]}>
+      <Logo />
+      <SearchForecast></SearchForecast>
+    </div>
+  );
+}
+
+export default ForecastPage;
