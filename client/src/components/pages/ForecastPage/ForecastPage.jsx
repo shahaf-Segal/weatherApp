32e3e9fec2374d19043ffc4ab23b84330c93abc5
shahaf-Segal <shahaf100@outlook.co.il>
2024-08-09 20:32:26 +0300

    fixed style and added search to forecast page

--- client/src/components/pages/ForecastPage/ForecastPage.jsx
+++ client/src/components/pages/ForecastPage/ForecastPage.jsx
@@ -1,12 +1,29 @@
-import Logo from "../../Logo/Logo";
+import axios from "axios";
+import { useState } from "react";
+import { SERVER_URL } from "../../../Constant/serverUrl";
 import SearchForecast from "../../SearchForecast/SearchForecast";
 import styles from "./ForecastPage.module.css";
 
 function ForecastPage() {
+  const [forecastData, setForecastData] = useState();
+
+  const fetchForecastData = async (searchQuery) => {
+    setForecastData();
+    try {
+      const forecastResp = await axios.get(`${SERVER_URL}${searchQuery}`);
+      console.log(forecastResp.data);
+      setForecastData(forecastResp.data);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
   return (
     <div className={styles["forecast-container"]}>
-      <Logo />
-      <SearchForecast></SearchForecast>
+      <SearchForecast
+        searchFunc={fetchForecastData}
+        locationData={forecastData?.location}
+      ></SearchForecast>
     </div>
   );
 }
