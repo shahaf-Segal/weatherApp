69e6beddbe3c59d89a164cdf81c1f280b81a973e
shahaf-Segal <shahaf100@outlook.co.il>
2024-08-09 14:32:00 +0300

    started with initializing front

---
+++ client/src/components/Logo/Logo.jsx
@@ -0,0 +1,11 @@
+import logoSvg from "/logo.svg";
+
+function Logo() {
+  return (
+    <div className="App">
+      <img src={logoSvg} alt="Your SVG" />
+    </div>
+  );
+}
+
+export default Logo;
