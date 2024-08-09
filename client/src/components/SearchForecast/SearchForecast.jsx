e618c5e47c1e1a7f0c0d06c6f519178dde7271c1
shahaf-Segal <shahaf100@outlook.co.il>
2024-08-09 16:15:49 +0300

    style changes

--- client/src/components/SearchForecast/SearchForecast.jsx
+++ client/src/components/SearchForecast/SearchForecast.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import Logo from "../Logo/Logo";
+import styles from "./SearchForecast.module.css";
 
 function SearchForecast() {
-  return <div>searchForecast</div>;
+  return (
+    <div className={styles["search-forecast"]}>
+      <div className={styles["logo-container"]}>
+        <Logo />
+      </div>
+      <div className={styles["search-inner-container"]}></div>
+      <section>Use our weather app to see the weather around the world</section>
+    </div>
+  );
 }
 
 export default SearchForecast;
